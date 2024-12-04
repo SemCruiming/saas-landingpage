7196288fe40b7a7d031803c71ddebeaffc156626
Sem Cruiming <semcruiming@gmail.com>
2024-12-04 21:24:52 +0100

    initial commit

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -9,8 +9,6 @@ import avatar7 from "@/assets/avatar-7.png";
 import avatar8 from "@/assets/avatar-8.png";
 import avatar9 from "@/assets/avatar-9.png";
 import Image from "next/image";
-import test from "node:test";
-import { twMerge } from "tailwind-merge";
 import { motion } from 'framer-motion'
 import React from "react";
 
